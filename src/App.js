a9469ca0833234be789ff5c729f51ddc0c48231e
duxinyues <yongyuan253015@gmail.com>
2021-07-28 23:10:26 +0800

    删除antd框架

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 /*
  * @Author: yongyuan at <yongyuan253015@gmail.com>
  * @Date: 2021-07-18 10:38:30
- * @LastEditTime: 2021-07-18 14:59:02
+ * @LastEditTime: 2021-07-28 23:06:52
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: 
  * @FilePath: \blog\src\App.js
  * 
  */
-
-import { Layout } from "antd";
+import React from "react"
 import Head from "./components/Head";
-import SiderCustom from "./components/SiderCustom"
-const {  Content } = Layout;
+import SiderCustom from "./components/SiderCustom";
+import ArticleList from "./components/list"
 function App() {
-   return <Layout>
+   return <React.Fragment>
       <Head />
       <article>
          <SiderCustom />
-         <Content>
-            
-         </Content>
+         <ArticleList />
+
       </article>
-   </Layout>
+   </React.Fragment>
 }
 
 export default App;
---
+++ src/components/list.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+function ArticleList() {
+    return <main class="r_box">
+        <li><i>这是什么</i>
+            <h3><a href="/">标题</a></h3>
+            <p>内容简版</p>
+        </li>
+        <li><i>这是什么</i>
+            <h3><a href="/">标题</a></h3>
+            <p>内容简版</p>
+        </li>
+        <li><i>这是什么</i>
+            <h3><a href="/">标题</a></h3>
+            <p>内容简版</p>
+        </li>
+    </main>
+}
+
+export default ArticleList
